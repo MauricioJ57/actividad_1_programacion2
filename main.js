903528552c8a5a87ac6e1b56c0620be8e67c839e
@mauricio-jacki <mauriciojacki@gmail.com>
2025-08-13 15:58:31 -0300

    se quito el template del repo

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import HelloWorldScene from "./scenes/HelloWorldScene.js";
+import BreakBrickScene from "./scenes/game.js";
 
-// Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -24,11 +23,7 @@ const config = {
       debug: true,
     },
   },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+  scene: [BreakBrickScene],
 };
 
-// Create a new Phaser game instance
 window.game = new Phaser.Game(config);
---
+++ scenes/game.js
@@ -0,0 +1,21 @@
+export default class BreakBrickScene extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
+
+  init() {
+
+  }
+
+  preload() {
+    
+  }
+
+  create() {
+  
+  }
+
+  update() {
+  
+  }
+}
