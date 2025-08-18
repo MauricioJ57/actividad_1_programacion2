1ad9d4c60286a30504884a518ebebe4eca2a3986
@mauricio-jacki <mauriciojacki@gmail.com>
2025-08-18 00:42:44 -0300

    se agregaron imagenes y los preload

--- scenes/game.js
+++ scenes/game.js
@@ -8,11 +8,17 @@ export default class BreakBrickScene extends Phaser.Scene {
   }
 
   preload() {
-    
+    this.load.image("pelota", "public/assets/pelota de break bricks.png")
+    this.load.image("plataforma", "public/assets/plataforma de break bricks.png")
+    this.load.image("rectangulo", "public/assets/rectangulo rompible(bb).png")
+    this.load.image("fondo", "public/assets/fondo de portada.jpg")
   }
 
   create() {
-  
+    this.add.image(400, 300, "fondo");
+    this.add.image(400, 300, "pelota");
+    this.add.image(400, 500, "plataforma");
+    this.add.image(400, 200, "rectangulo");
   }
 
   update() {
